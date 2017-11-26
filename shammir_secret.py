17be06c2c19d7d735069b1f62100cb6283f1b4a9
chuseuiti <jesuscardenes@hotmail.com>
2017-11-26 02:30:28 -0500

    Update shammir secret sharing example code.

---
+++ shammir_secret.py
@@ -0,0 +1,16 @@
+from secretsharing import PlaintextToHexSecretSharer
+
+shares = PlaintextToHexSecretSharer.split_secret("{'address':'Sainte Catherine' , 'telephone': 524-857-9127}", 2, 3)
+
+print('shares:', shares)
+
+print('Recover of information from 1 and 2:' + PlaintextToHexSecretSharer.recover_secret(shares[0:2]))
+print('Recover of information from 1 and 3:' + PlaintextToHexSecretSharer.recover_secret([shares[0],shares[2]]))
+print('Recover of information from 2 and 3:' + PlaintextToHexSecretSharer.recover_secret(shares[1:3]))
+try:
+    print('Recover of information from 2 and 3:' + PlaintextToHexSecretSharer.recover_secret(shares[1]))
+except:
+    print('Not enough available information')
+    
+
+
