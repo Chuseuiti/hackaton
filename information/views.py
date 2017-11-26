59c52848b47e206f8e3859aa72d3c496258c83a8
chuseuiti <jesuscardenes@hotmail.com>
2017-11-25 23:13:59 -0500

    Update private and public key generation

--- information/views.py
+++ information/views.py
@@ -2,18 +2,48 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 import json
 # Create your views here.
-
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.backends import default_backend
 
 def document_encrypt(request):
 
-	return HttpResponse(json.dumps({'response':'Documents encrypted saved on the database.'}), status = 200)
+    # Save encrypt information in database
+    return HttpResponse(json.dumps({'response':'Documents encrypted saved on the database.'}), status = 200)
 
 def document_raw(request):
+    # Get private key from the database for the user
 
-	return HttpResponse(json.dumps({'response':'Documents correctly unencrypted.'}), status = 200)
+
+    # Decrypt information
+    plaintext = private_key.decrypt(
+         ciphertext,
+         padding.OAEP(
+             mgf=padding.MGF1(algorithm=hashes.SHA1()),
+             algorithm=hashes.SHA1(),
+             label=None
+         )
+     )
+    print(plaintext)    
+    return HttpResponse(json.dumps({'response':'Documents correctly unencrypted.'}), status = 200)
 
 def information_request(request):
+    # generate private/public key pair
+    key = rsa.generate_private_key(backend=default_backend(), public_exponent=65537, \
+    key_size=2048)
 
-	return HttpResponse(json.dumps({'response':'home_address', 'message':'Please provide the information requested.','public_key':'nuascgb3unugnun2348743n6234084nciin'}), status = 200)
+    # Get public key in OpenSSH format
+    public_key = key.public_key().public_bytes(serialization.Encoding.OpenSSH, \
+    serialization.PublicFormat.OpenSSH)
+    print(public_key.decode('utf-8'))
+    # Get private Key 
+    private_key = key.private_bytes(encoding=serialization.Encoding.PEM,
+    format=serialization.PrivateFormat.TraditionalOpenSSL,
+    encryption_algorithm=serialization.NoEncryption())
+    print(private_key.decode('utf-8'))
+
+    # Save in database
+
+    return HttpResponse(json.dumps({'response':'home_address', 'message':'Please provide the information requested.','public_key':public_key.decode('utf-8')}), status = 200)
 
 
--- registration/views.py
+++ registration/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
+import random
+import json
+import time
 # Create your views here.
 
 
 def register(request):
 
-	return HttpResponse(json.dumps({'response':'User registered into the system'}), status = 200)
+    # Save into the system
+
+    return HttpResponse(json.dumps({'response':'User registered into the system'}), status = 200)
 
 def authentication(request):
 
-	return HttpResponse(json.dumps({'response':'nsackueynw8c9y8cn134cnc4n8cmpq'}), status = 200)
+    #Random number generator
+    hexdigits = "0123456789ABCDEF"
+    random_digits = "".join([ hexdigits[random.randint(0,0xF)] for _ in range(64) ])
+    #Save into the database: Token + Timestamp + Time in seconds
+    timestamp = time.time()
+    expire = 3600 # seconds
+    #Forward to the client
+    return HttpResponse(json.dumps({'response':random_digits}), status = 200)
